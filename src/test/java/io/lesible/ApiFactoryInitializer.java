72d29ec9845ceaf3fc4683ba80bc5fdbcf821f6b
Lesible <>
2021-07-27 15:22:53 +0800

    no modify

--- src/test/java/io/lesible/ApiFactoryInitializer.java
+++ src/test/java/io/lesible/ApiFactoryInitializer.java
@@ -7,6 +7,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,8 +18,14 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApiFactoryInitializer {
 
-    //    public static final String GLOBAL_TOKEN = "2f0136ed-67ea-4d53-857b-7dd6e739f3ce";
-    public static final String GLOBAL_TOKEN = "9946cc3d-2641-49ef-8b74-ce20d0e26394";
+    //        public static final String GLOBAL_TOKEN = "6c2ec233-a271-4b74-a42c-8673cb028b51";
+//        public static final String GLOBAL_TOKEN = "84752931-68ff-4d22-989a-65b220587676";
+//public static final String GLOBAL_TOKEN = "d322a996-c014-4c6c-a848-d33508f95948";
+    // 窝小芽
+//    public static final String GLOBAL_TOKEN = "7a660b6e-35f6-461e-adb4-3ced59c74fda";
+    public static final String GLOBAL_TOKEN = "61a00565-744e-4bda-adac-96b8ee73fd1a";
+
+
     public static OrderApi ORDER_API;
     public static ProductApi PRODUCT_API;
     public static CommentApi COMMENT_API;
@@ -27,6 +34,7 @@ public class ApiFactoryInitializer {
     public static MarketingApi MARKETING_API;
     public static BillingApi BILLING_API;
     public static UserApi USER_API;
+    public static SkuApi SKU_API;
 
     static {
         Authorization authorization = new Authorization("6873668517658347022", "601f6adf-f7df-42bf-8789-1b3a90979c50");
@@ -37,6 +45,7 @@ public class ApiFactoryInitializer {
                 .addInterceptor(interceptor)
                 .build();
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://openapi-fxg.jinritemai.com/")
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(JacksonConverterFactory.create(ApiFactory.OBJECT_MAPPER))
                 .client(client).build();
         ApiFactory API_FACTORY = new ApiFactory(retrofit, authorization);
@@ -48,6 +57,7 @@ public class ApiFactoryInitializer {
         MARKETING_API = API_FACTORY.generateApi(MarketingApi.class);
         BILLING_API = API_FACTORY.generateApi(BillingApi.class);
         USER_API = API_FACTORY.generateApi(UserApi.class);
+        SKU_API = API_FACTORY.generateApi(SkuApi.class);
     }
 
 
--- src/test/java/io/lesible/OrderApiTestCase.java
+++ src/test/java/io/lesible/OrderApiTestCase.java
@@ -1,21 +1,27 @@
 package io.lesible;
 
 import io.lesible.api.OrderApi;
-import io.lesible.model.constant.MethodConstant;
+import io.lesible.common.constant.MethodConstants;
 import io.lesible.model.request.DySignRequest;
-import io.lesible.model.request.order.OrderOrderDetailParam;
-import io.lesible.model.request.order.OrderSearchListParam;
+import io.lesible.model.request.order.*;
 import io.lesible.model.response.DyResult;
+import io.lesible.model.response.order.DeSensitiveResult;
 import io.lesible.model.response.order.OrderPageInfo;
+import io.lesible.model.response.order.ShopOrderDetail;
 import io.lesible.model.response.order.ShopOrderDetailInfo;
+import io.lesible.util.JsonUtil;
 import io.lesible.util.ParamUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import retrofit2.Call;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,18 +41,24 @@ public class OrderApiTestCase {
     @SneakyThrows
     public void orderSearchList() {
         ZoneOffset defaultZoneOffset = ZoneOffset.of("+8");
-        LocalDateTime yesterday = LocalDateTime.now().minusDays(2L);
-        LocalDateTime localDateTime = LocalDateTime.now();
-        long end = localDateTime.toEpochSecond(defaultZoneOffset);
-        long begin = yesterday.toEpochSecond(defaultZoneOffset);
+        LocalDateTime oldTime = LocalDateTime.of(2021, 7, 23, 10, 51);
+        LocalDateTime newTime = LocalDateTime.of(2021, 7, 26, 10, 51);
+//        LocalDateTime oldTime = LocalDateTime.now().minusDays(10L);
+//        LocalDateTime newTime = LocalDateTime.now();
+        long begin = oldTime.toEpochSecond(defaultZoneOffset);
+        log.info("begin: {}", begin);
+        long end = newTime.toEpochSecond(defaultZoneOffset);
+        log.info("end: {}", end);
         OrderSearchListParam param = OrderSearchListParam.builder()
-                .page(0).size(100)
-                .createTimeStart(begin)
-                .createTimeEnd(end)
+                .updateTimeStart(begin)
+                .updateTimeEnd(end)
+                .page(500).size(100)
+                .orderBy("update_time")
+                .orderAsc(true)
                 .build();
         DySignRequest<OrderSearchListParam> request = DySignRequest.<OrderSearchListParam>builder()
                 .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
-                .businessParam(param).method(MethodConstant.ORDER_SEARCH_LIST).build();
+                .businessParam(param).method(MethodConstants.ORDER_SEARCH_LIST).build();
         Map<String, String> paramMap = ParamUtil.buildParamMap(request);
         Call<DyResult<OrderPageInfo>> dyResultCall = orderApi.searchList(paramMap);
         DyResult<OrderPageInfo> body = dyResultCall.execute().body();
@@ -59,14 +71,85 @@ public class OrderApiTestCase {
     @Test
     @SneakyThrows
     public void orderOrderDetail() {
-        OrderOrderDetailParam param = OrderOrderDetailParam.builder().shopOrderId(4807261687878395536L).build();
+        OrderOrderDetailParam param = OrderOrderDetailParam.builder().shopOrderId(4807510692898733727L).build();
         DySignRequest<OrderOrderDetailParam> request = DySignRequest.<OrderOrderDetailParam>builder()
                 .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
-                .businessParam(param).method(MethodConstant.ORDER_ORDER_DETAIL).build();
+                .businessParam(param).method(MethodConstants.ORDER_ORDER_DETAIL).build();
         Map<String, String> paramMap = ParamUtil.buildParamMap(request);
         Call<DyResult<ShopOrderDetailInfo>> dyResultCall = orderApi.orderDetail(paramMap);
         DyResult<ShopOrderDetailInfo> orderDetailInfo = dyResultCall.execute().body();
-        log.info("orderDetailInfo: {}", orderDetailInfo);
+        log.info("orderDetailInfo: {}", JsonUtil.jsonValue(orderDetailInfo));
+    }
+
+    /**
+     * 订单解密
+     */
+    @Test
+    @SneakyThrows
+    public void orderBatchDecrypt() {
+        OrderOrderDetailParam param = OrderOrderDetailParam.builder().shopOrderId(4807510692898733727L).build();
+        DySignRequest<OrderOrderDetailParam> request = DySignRequest.<OrderOrderDetailParam>builder()
+                .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
+                .businessParam(param).method(MethodConstants.ORDER_ORDER_DETAIL).build();
+        Map<String, String> paramMap = ParamUtil.buildParamMap(request);
+        Call<DyResult<ShopOrderDetailInfo>> dyResultCall = orderApi.orderDetail(paramMap);
+        DyResult<ShopOrderDetailInfo> orderDetailInfo = dyResultCall.execute().body();
+        if (orderDetailInfo != null && orderDetailInfo.isSuccess()) {
+            ShopOrderDetail shopOrderDetail = orderDetailInfo.getData().getShopOrderDetail();
+            String encryptPostReceiver = shopOrderDetail.getEncryptPostReceiver();
+            String encryptPostTel = shopOrderDetail.getEncryptPostTel();
+            Long orderId = shopOrderDetail.getOrderId();
+            List<CipherInfo> cipherInfos = Arrays.asList(
+                    CipherInfo.builder().authId(orderId).cipherText(encryptPostReceiver).build(),
+                    CipherInfo.builder().authId(orderId).cipherText(encryptPostTel).build());
+            OrderBatchDecryptParam innerParam = OrderBatchDecryptParam.builder().cipherInfos(cipherInfos).build();
+            DySignRequest<OrderBatchDecryptParam> innerRequest = DySignRequest.<OrderBatchDecryptParam>builder()
+                    .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
+                    .businessParam(innerParam).method(MethodConstants.ORDER_BATCH_DECRYPT).build();
+            Map<String, String> innerParamMap = ParamUtil.buildParamMap(innerRequest);
+            Call<String> result = orderApi.batchDecrypt(innerParamMap);
+            String body = result.execute().body();
+            log.info("body: {}", body);
+        }
+    }
+
+    /**
+     * 订单脱敏
+     */
+    @Test
+    @SneakyThrows
+    public void orderBatchSensitive() {
+        OrderOrderDetailParam param = OrderOrderDetailParam.builder().shopOrderId(4807510692898733727L).build();
+        DySignRequest<OrderOrderDetailParam> request = DySignRequest.<OrderOrderDetailParam>builder()
+                .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
+                .businessParam(param).method(MethodConstants.ORDER_ORDER_DETAIL).build();
+        Map<String, String> paramMap = ParamUtil.buildParamMap(request);
+        Call<DyResult<ShopOrderDetailInfo>> dyResultCall = orderApi.orderDetail(paramMap);
+        DyResult<ShopOrderDetailInfo> orderDetailInfo = dyResultCall.execute().body();
+        if (orderDetailInfo != null && orderDetailInfo.isSuccess()) {
+            ShopOrderDetail shopOrderDetail = orderDetailInfo.getData().getShopOrderDetail();
+            String encryptPostReceiver = shopOrderDetail.getEncryptPostReceiver();
+            String encryptPostTel = shopOrderDetail.getEncryptPostTel();
+            Long orderId = shopOrderDetail.getOrderId();
+            List<CipherInfo> cipherInfos = Arrays.asList(
+                    CipherInfo.builder().authId(orderId).cipherText(encryptPostReceiver).build(),
+                    CipherInfo.builder().authId(orderId).cipherText(encryptPostTel).build());
+            OrderBatchSensitiveParam innerParam = OrderBatchSensitiveParam.builder().cipherInfos(cipherInfos).build();
+            DySignRequest<OrderBatchSensitiveParam> innerRequest = DySignRequest.<OrderBatchSensitiveParam>builder()
+                    .accessToken(ApiFactoryInitializer.GLOBAL_TOKEN)
+                    .businessParam(innerParam).method(MethodConstants.ORDER_BATCH_SENSITIVE).build();
+            Map<String, String> innerParamMap = ParamUtil.buildParamMap(innerRequest);
+            Call<DyResult<DeSensitiveResult>> result = orderApi.batchSensitive(innerParamMap);
+            DyResult<DeSensitiveResult> body = result.execute().body();
+            log.info("body: {}", JsonUtil.jsonValue(body));
+        }
+    }
+
+    @Test
+    public void time() {
+        Instant i = Instant.ofEpochSecond(1627176322L);
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(i, ZoneId.of("+8"));
+        log.info("localDateTime: {}", localDateTime);
     }
 
 }
